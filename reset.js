25d0e887e11abebefe64542c467161228b4aea8b
wjschr18 <wjschr18@stlawu.edu>
2020-04-27 17:19:41 -0400

    teams.js and schema

--- reset.js
+++ reset.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose');
 const connect = require('./db');
-const Course = require('./models/stats');
+// const Course = require('./models/stats');
+const Team = require('./teams');
 
 // Connect to the database
 connect();
 
 // Model a collection of courses
-const courses = [
-  new Course({_id: });
-];
+// const courses = [
+//   new Course({_id: });
+// ];
+const teams = [
+  new Team({
+    id: 'testTeam',
+    teamName: 'Test Team',
+    teamScore: 0
+  });
+]
+
 // Reset the database
 mongoose.connection.dropDatabase()
-  .then(() => Promise.all(courses.map(stats => stats.save())))
+  // .then(() => Promise.all(courses.map(stats => stats.save())))
+  .then(() => Promise.all(teams.map(teams => teams.save())))
   .then(() => mongoose.connection.close())
   .then(() => console.log('Database is ready.'))
   .catch(error => console.error(error.stack));
--- schema.js
+++ schema.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose');
 
-const Stats = new mongoose.Schema({
-  _id: String,
-  points: [String],
-  fantasypoints: [String],
-  Owner:[String]
+// const Stats = new mongoose.Schema({
+//   _id: String,
+//   points: [String],
+//   fantasypoints: [String],
+//   Owner:[String]
+// });
+
+const Team = new mongoose.Schema({
+  teamName: String,
+  teamScore: Number
 });
 
-module.exports = mongoose.model('Stats', stats);
+// module.exports = mongoose.model('Stats', stats);
+module.exports = mongoose.model('Team', Team);
