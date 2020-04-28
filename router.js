d7e228d9741f8766f31ca83f68beb861aa122be5
wjschr18 <wjschr18@stlawu.edu>
2020-04-28 15:47:20 -0400

    more comments

--- router.js
+++ router.js
@@ -14,11 +14,11 @@ const authorize = function(request, response, next) {
 };
 
 // Handle course requests
-router.get('/stats', stats.index);
-router.get('/stats/:id', stats.retrieve);
-router.post('/stats', authorize, stats.create);
-router.delete('/stats/:id', authorize, stats.delete);
-router.put('/stats/:id', authorize, stats.update);
+// router.get('/stats', stats.index);
+// router.get('/stats/:id', stats.retrieve);
+// router.post('/stats', authorize, stats.create);
+// router.delete('/stats/:id', authorize, stats.delete);
+// router.put('/stats/:id', authorize, stats.update);
 
 // Export the router
 module.exports = router;
