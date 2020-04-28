f96e85265f6bcfe9ab3711b4f33f3bf57142a571
wjschr18 <wjschr18@stlawu.edu>
2020-04-28 15:40:17 -0400

    removed stage 2 code

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+// const session = require('express-session');
 const router = require('./router');
 const connect = require('./db');
 
@@ -16,13 +16,13 @@ app.set('views', './views');
 // Parse request bodies like query strings
 app.use(express.urlencoded({extended: false}));
 
-// Generate a session for each client
-app.use(session({
-  name: 'catalog', // Name of client cookies
-  secret: 'temporary', // Password for client cookies
-  resave: false, // Recommended setting
-  saveUninitialized: false // Recommended setting
-}));
+// // Generate a session for each client
+// app.use(session({
+//   name: 'catalog', // Name of client cookies
+//   secret: 'temporary', // Password for client cookies
+//   resave: false, // Recommended setting
+//   saveUninitialized: false // Recommended setting
+// }));
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
@@ -37,28 +37,28 @@ app.use(function(request, response, next) {
   next();
 });
 
-// Redirect from the home page
-app.get('/', function(request, response) {
-  response.redirect('/courses');
-});
-
-// Enter admin mode and return to the previous page
-app.get('/login', function(request, response) {
-  request.session.admin = true;
-  response.redirect('back');
-});
-
-// Exit admin mode and return to the previous page
-app.get('/logout', function(request, response) {
-  request.session.admin = false;
-  response.redirect('back');
-});
-
-// Make the mode available in all views
-app.use(function(request, response, next) {
-  response.locals.admin = request.session.admin;
-  next();
-});
+// // Redirect from the home page
+// app.get('/', function(request, response) {
+//   response.redirect('/courses');
+// });
+//
+// // Enter admin mode and return to the previous page
+// app.get('/login', function(request, response) {
+//   request.session.admin = true;
+//   response.redirect('back');
+// });
+//
+// // Exit admin mode and return to the previous page
+// app.get('/logout', function(request, response) {
+//   request.session.admin = false;
+//   response.redirect('back');
+// });
+//
+// // Make the mode available in all views
+// app.use(function(request, response, next) {
+//   response.locals.admin = request.session.admin;
+//   next();
+// });
 
 // Route content requests
 app.use('/', router);
